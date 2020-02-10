4d3556dd16f7ce452cf715de6b3fb03e4f31f24f
Munandermaan <choudhary.munander.maan@gmail.com>
2020-02-10 12:22:17 +0530

    Streaming via gRPC

---
+++ src/main/java/com/knoldus/grpc/server/GreetingServer.java
@@ -0,0 +1,28 @@
+package com.knoldus.grpc.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new GreetServiceImpl())
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received Shutdown Request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        server.awaitTermination();
+    }
+
+}
